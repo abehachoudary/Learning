ac432541f0a1adfe8d910e289bc2f0ac01aa596f
abehachoudary <abehachoudary@gmail.com>
2025-02-13 15:08:24 -0500

    New file last one had some error

--- PYCoding.py
+++
@@ -1,8 +0,0 @@
-nums = (1,4,9,16,25,36,49,64,81,100)
-
-for val in nums:
-    if(val == 9):
-        print(f"{val} found")
-        break
-else:
-    print("9 not found")
---
+++ coding.py
@@ -0,0 +1,6 @@
+num_list = ["hello", "world", "python", "is", "awesome"]
+def list_len():
+    length = len(num_list)
+    return len(num_list)
+
+print(list_len())
